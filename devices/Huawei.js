fe324ddb4b69059d398bc98b0dac6799372484ef
maniacx <macs18max@gmail.com>
2024-08-19 00:25:33 -0700

    Huawei: adapt to exitCode

--- devices/Huawei.js
+++ devices/Huawei.js
@@ -4,7 +4,7 @@ import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import * as Helper from '../lib/helper.js';
 
-const {fileExists, readFile, runCommandCtl} = Helper;
+const {exitCode, fileExists, readFile, runCommandCtl} = Helper;
 
 const HUAWEI_PATH = '/sys/devices/platform/huawei-wmi/charge_control_thresholds';
 
@@ -53,29 +53,38 @@ export const HuaweiSingleBattery = GObject.registerClass({
     }
 
     async setThresholdLimit(chargingMode) {
-        this._status = 0;
         this._limitValue = ['0', '0'];
         this._endValue = this._settings.get_int(`current-${chargingMode}-end-threshold`);
         this._startValue = this._settings.get_int(`current-${chargingMode}-start-threshold`);
-        if (this._status === 0) {
-            if (this._verifyThreshold())
-                return this._status;
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        const [status] = await runCommandCtl(this.ctlPath, 'HUAWEI', `${this._endValue}`, `${this._startValue}`, null);
+        if (status === exitCode.ERROR) {
+            this.emit('threshold-applied', 'failed');
+            return exitCode.ERROR;
         }
-        [this._status] = await runCommandCtl(this.ctlPath, 'HUAWEI', `${this._endValue}`, `${this._startValue}`, null);
-        if (this._status === 0) {
-            if (this._verifyThreshold())
-                return this._status;
-        }
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
         if (this._delayReadTimeoutId)
             GLib.source_remove(this._delayReadTimeoutId);
         this._delayReadTimeoutId = null;
 
-        this._delayReadTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
-            this._reVerifyThreshold();
-            this._delayReadTimeoutId = null;
-            return GLib.SOURCE_REMOVE;
+        await new Promise(resolve => {
+            this._delayReadTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
+                resolve();
+                return GLib.SOURCE_REMOVE;
+            });
         });
-        return this._status;
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        this.emit('threshold-applied', 'failed');
+        return exitCode.ERROR;
     }
 
     _verifyThreshold() {
@@ -87,14 +96,6 @@ export const HuaweiSingleBattery = GObject.registerClass({
             return true;
         }
         return false;
-    }
-
-    _reVerifyThreshold() {
-        if (this._status === 0) {
-            if (this._verifyThreshold())
-                return;
-        }
-        this.emit('threshold-applied', 'failed');
     }
 
     destroy() {
