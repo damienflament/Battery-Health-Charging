2a6fb91d3179f94f360e17ca8ee0208dcdd17d66
maniacx <macs18max@gmail.com>
2024-08-19 00:25:25 -0700

    Acer: adapt to exitCode

--- devices/Acer.js
+++ devices/Acer.js
@@ -4,7 +4,7 @@ import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import * as Helper from '../lib/helper.js';
 
-const {fileExists, readFileInt, runCommandCtl} = Helper;
+const {exitCode, fileExists, readFileInt, runCommandCtl} = Helper;
 
 const ACER_PATH = '/sys/bus/wmi/drivers/acer-wmi-battery/health_mode';
 
@@ -37,30 +37,39 @@ export const AcerSingleBattery = GObject.registerClass({
     }
 
     async setThresholdLimit(chargingMode) {
-        this._status = 0;
-        this._chargingMode = chargingMode;
-        if (this._chargingMode === 'ful')
+        if (chargingMode === 'ful')
             this._healthMode = 0;
-        else if (this._chargingMode === 'max')
+        else if (chargingMode === 'max')
             this._healthMode = 1;
+
         if (this._verifyThreshold())
-            return this._status;
-        [this._status] = await runCommandCtl(this.ctlPath, 'ACER', `${this._healthMode}`, null, null);
-        if (this._status === 0) {
-            if (this._verifyThreshold())
-                return this._status;
+            return exitCode.SUCCESS;
+
+        const [status] = await runCommandCtl(this.ctlPath, 'ACER', `${this._healthMode}`, null, null);
+        if (status === exitCode.ERROR) {
+            this.emit('threshold-applied', 'failed');
+            return exitCode.ERROR;
         }
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
 
         if (this._delayReadTimeoutId)
             GLib.source_remove(this._delayReadTimeoutId);
         this._delayReadTimeoutId = null;
 
-        this._delayReadTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
-            this._reVerifyThreshold();
-            this._delayReadTimeoutId = null;
-            return GLib.SOURCE_REMOVE;
+        await new Promise(resolve => {
+            this._delayReadTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 200, () => {
+                resolve();
+                return GLib.SOURCE_REMOVE;
+            });
         });
-        return this._status;
+
+        if (this._verifyThreshold())
+            return exitCode.SUCCESS;
+
+        this.emit('threshold-applied', 'failed');
+        return exitCode.ERROR;
     }
 
     _verifyThreshold() {
@@ -74,14 +83,6 @@ export const AcerSingleBattery = GObject.registerClass({
             return true;
         }
         return false;
-    }
-
-    _reVerifyThreshold() {
-        if (this._status === 0) {
-            if (this._verifyThreshold())
-                return;
-        }
-        this.emit('threshold-applied', 'failed');
     }
 
     destroy() {
